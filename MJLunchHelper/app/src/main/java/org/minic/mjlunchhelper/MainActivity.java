404b194bff03439995017122c45e9087d0a91550
acabar9 <acabar9@naver.com>
2017-09-03 22:51:32 +0900

    Lunchhelper first

---
+++ MJLunchHelper/app/src/main/java/org/minic/mjlunchhelper/MainActivity.java
@@ -0,0 +1,45 @@
+package org.minic.mjlunchhelper;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_1:
+                    mTextMessage.setText("밥집 랜덤");
+                    return true;
+                case R.id.navigation_2:
+                    mTextMessage.setText("술집 랜덤");
+                    return true;
+                case R.id.navigation_3:
+                    mTextMessage.setText("리스트 추가");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+
+        mTextMessage = (TextView) findViewById(R.id.navname);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
